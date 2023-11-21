a2d8c7a3700074fa32daf4fc321f6221acb6f999
As1mov <realmofthemadzueira@gmail.com>
2023-11-21 11:18:24 -0300

    refactor:refazer login.php

--- login.php
+++ login.php
@@ -7,32 +7,7 @@
 </head>
 <body>
     <?php 
-        session_start();
-
-        if(empty($_POST) or (empty($_POST["usuario"]) or (empty($_POST["senha"]))))
-         {
-            print'<script>location.href=index.html';
-         }
-        include ("banco.php");
-
-         $usuario = $_POST["usuario"];
-         $senha = $_POST["senha"];
-
-         $sql = "SELECT * FROM usuarios
-         WHERE usuario = '{$usuario}'
-         AND senha = '{$senha}'";
-
-         $resposta = $conexao->query($sql);
-         $row = $resposta->fetch_object();
-         $quantidade = $resposta->num_rows;
-
-         if($quantidade > 0){
-            $_SESSION["usuario"] = $usuario;
-            $_SESSION["nome"] = $row->nome;
-         }else{
-            print"<script>alert('Usuario e/ou senha incorretos')</script>";
-            print"<script>location.href=index.html</script>";
-         }
+        
     ?>
 </body>
 </html>
