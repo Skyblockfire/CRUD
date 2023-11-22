18c8e00f27bb3adcb1ba2f491e018aa74977f82d
As1mov <realmofthemadzueira@gmail.com>
2023-11-22 10:38:16 -0300

    feat:criar funcoes atualizar e deletar.

--- userDAO.php
+++ userDAO.php
@@ -24,14 +24,37 @@ class userDAO{
         $endereco = $user->getEndereco();
         $carro = $user->getCarro();
 
-
         $Array = array($admin,$nome,$sobrenome,$senha,$cpf,$cnh,$telefone,$endereco,$carro);
 
         $criar_usuario = $this->banco->prepare('INSERT INTO usuario(admin,nome,sobrenome,senha,cpf,cnh,telefone,endereco,carro) VALUES(?,?,?,?,?,?,?,?,?);');
 
         $criar_usuario->execute($Array);
+    }
 
+    public function alterar_usuario($user){
+        $admin = $user->getAdmin();
+        $nome = $user->getNome();
+        $sobrenome = $user->getSobrenome();
+        $senha = $user->getSenha();
+        $cpf = $user->getCpf();
+        $cnh = $user->getCnh();;
+        $telefone = $user->getTelefone();
+        $endereco = $user->getEndereco();
+        $carro = $user->getCarro();
 
+        $Array = array($admin,$nome,$sobrenome,$senha,$cpf,$cnh,$telefone,$endereco,$carro);
+
+        $alterar_usuario = $this->banco->prepare('UPDATE usuario(admin,nome,sobrenome,senha,cpf,cnh,telefone,endereco,carro) VALUES(?,?,?,?,?,?,?,?,?);');
+
+        $alterar_usuario->execute($Array);
+    }
+
+    public function deletar_usuario($cpf){
+        $Array = array($cpf);
+
+        $deletar_usuario = $this->banco->prepare('DELETE FROM usuario WHERE CPF = ?;');
+
+        $deletar_usuario->execute($Array);
     }
 }
 ?>
