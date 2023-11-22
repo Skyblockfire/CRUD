067320341045a6c413c8fcd274dd96e484a39c5d
As1mov <realmofthemadzueira@gmail.com>
2023-11-22 11:14:04 -0300

    feat:incluir bootstrap.

--- login.php
+++ login.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-</head>
-<body>
-    <?php 
-        
-    ?>
-</body>
-</html>
+<?php 
+    $CPF = $_POST['CPF'];
+    $Senha = $_POST['Senha'];
+?>
